5b20ce78251f3d7b75eaaa9e3caf5b53b42a65d4
Vu <hoaivubk@gmail.com>
2026-07-10 23:57:16 +0700

    feat(model-routing): export routing runtime API

--- vnalpha/src/vnalpha/model_routing/__init__.py
+++ vnalpha/src/vnalpha/model_routing/__init__.py
@@ -1,19 +1,43 @@
 from vnalpha.model_routing.config import ModelRoutingConfig
 from vnalpha.model_routing.integration import GatewayRouteRequest, resolve_gateway_route
-from vnalpha.model_routing.models import ModelProfile, ModelRouteDecision
-from vnalpha.model_routing.overrides import ModelRouteOverride, resolve_override
+from vnalpha.model_routing.models import (
+    ModelProfile,
+    ModelRouteDecision,
+    ModelRouteStage,
+    ModelTaskType,
+)
+from vnalpha.model_routing.overrides import (
+    DEFAULT_OVERRIDE_STORE,
+    ModelOverrideStore,
+    ModelRouteOverride,
+    resolve_override,
+    resolve_override_with_source,
+)
 from vnalpha.model_routing.policy import default_profile_for_stage, profile_for
-from vnalpha.model_routing.resolver import resolve_model_route
+from vnalpha.model_routing.resolver import (
+    decision_for_profile,
+    fallback_route_decisions,
+    resolve_model_route,
+)
+from vnalpha.model_routing.runtime import get_last_route_decision
 
 __all__ = [
+    "DEFAULT_OVERRIDE_STORE",
     "GatewayRouteRequest",
+    "ModelOverrideStore",
     "ModelProfile",
     "ModelRouteDecision",
     "ModelRouteOverride",
+    "ModelRouteStage",
     "ModelRoutingConfig",
+    "ModelTaskType",
+    "decision_for_profile",
     "default_profile_for_stage",
+    "fallback_route_decisions",
+    "get_last_route_decision",
     "profile_for",
     "resolve_gateway_route",
     "resolve_model_route",
     "resolve_override",
+    "resolve_override_with_source",
 ]
